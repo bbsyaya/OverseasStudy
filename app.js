05cb0ce937e16672ca5a6c2f51ce8d330dc80b9d
xiaoluo <xiaoluo@mogujie.com>
2016-09-10 14:24:19 +0800

    feat:添加登录页面，引入ejs引擎

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+//加载管理员用户登录的路由
+var adminLogin = require('./routes/login/admin.js')
+// //加载小编用户登录的路由
+// var editorLogin = require('./routes/login/editor.js')
+// //加载文案撰写者的路由
+// var writeLogin = require('./routes/login/writer.js')
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.engine('.html',require('ejs')._express);
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,9 +30,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'common')));
+//加载相应的路由逻辑
 app.use('/', routes);
 app.use('/users', users);
+app.use('/admin/login',adminLogin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
