48c5251438f41991ef524d4027a2081d0cbf0bc6
xiaoluo <xiaoluo@mogujie.com>
2016-10-11 13:31:53 +0800

    feat:调整下结构

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ var customerService = require('./routes/customerService/customerService.js')
 var adminLogin = require('./routes/login/admin.js')
 
 var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// app.engine('.html',require('ejs')._express);
 app.set('view engine', 'ejs');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -35,16 +36,12 @@ app.use('/editor/index',editor)
 app.use('/writer/index',writer)
 app.use('/customerService/index',customerService)
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -54,8 +51,7 @@ if (app.get('env') === 'development') {
     });
   });
 }
-// production error handler
-// no stacktraces leaked to user
+
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
